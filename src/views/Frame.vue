2bfcbd4c6d8227be799b3e1b6d250a0043bf9781
Mark Okoh <markokoh@yahoo.co.uk>
2023-01-14 12:58:35 -0800

    Created data file, to store nft data.

--- src/views/Frame.vue
+++ src/views/Frame.vue
@@ -6,29 +6,21 @@
 
 <script setup>
 import { ref } from "vue";
-// import { db } from "../firebase/config";
 import { db } from "../firebase/config";
 import { doc, onSnapshot } from "firebase/firestore";
+import nfts from "../data/nfts.json";
 
 const imageUrl = ref("");
-
-const image1 =
-  "https://firebasestorage.googleapis.com/v0/b/textvote-7a52e.appspot.com/o/images%2FJasperArt_2022-09-04_16.10.11_1.png?alt=media&token=3fc2f3f0-dd36-4d9f-a201-4f60541bcf0a";
-const image2 =
-  "https://firebasestorage.googleapis.com/v0/b/textvote-7a52e.appspot.com/o/JasperArt_2022-09-04_16.16.22_2.png?alt=media&token=0c11e0c5-9bed-4b0a-87b0-a3df21865007";
-
-const imageAlt = ref("");
-const imageRef = ref(null);
 
 const getImage = (num) => {
   switch (num) {
     case 1:
       // code to be executed if expression is equal to value1
-      imageUrl.value = image1;
+      imageUrl.value = nfts.image1.URL;
       break;
     case 2:
       // code to be executed if expression is equal to value2
-      imageUrl.value = image2;
+      imageUrl.value = nfts.image2.URL;
       break;
     default:
       // code to be executed if expression does not match any of the cases
