73345d77259c1e2bfe30822c94b19a3da151d0a9
Mark Okoh <markokoh@yahoo.co.uk>
2023-01-14 16:11:31 -0800

    Replaced Vue 'Ref' with 'Reactive' to clean-up code (too many .value's)

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,27 +1,29 @@
 <template>
   <div class="image-container">
-    <h1>{{ title }}</h1>
-    <img :src="imageUrl" :alt="description" class="centered-image" />
-    <p>{{ description }}</p>
+    <h1>{{ state.title }}</h1>
+    <img :src="state.imageUrl" class="centered-image" />
+    <p>{{ state.description }}</p>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { reactive } from "vue";
 import nfts from "../data/nfts.json";
 import { db } from "../firebase/config";
 import { doc, onSnapshot } from "firebase/firestore";
 
-const title = ref("");
-const imageUrl = ref("");
-const description = ref(nfts.image1.description);
+const state = reactive({
+  title: "",
+  imageUrl: "",
+  description: "",
+});
 
 onSnapshot(doc(db, "shows", "gallery"), (doc) => {
   const { frame } = doc.data();
-  const art = Object.values(nfts)[frame - 1];
-  title.value = art.name;
-  imageUrl.value = art.URL;
-  description.value = art.description;
+  const nft = Object.values(nfts)[frame - 1];
+  state.title = nft.name;
+  state.imageUrl = nft.URL;
+  state.description = nft.description;
 });
 </script>
 
@@ -29,7 +31,6 @@ onSnapshot(doc(db, "shows", "gallery"), (doc) => {
 .image-container {
   display: flex;
   flex-direction: column;
-  /* align-items: start; */
   color: white;
 }
 
